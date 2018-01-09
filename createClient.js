8d6f035f90d1841b49d3fdc00978aecea0086a21
Romain Beaumont <romain.rom1@gmail.com>
2018-01-09 21:59:54 +0100

    improve code : move client2 to getMpq and fix step 16

--- createClient.js
+++ createClient.js
@@ -1,15 +1,14 @@
 const Client = require('./client');
 
-const Client2 = require('./client2');
-
 const fs = require('fs');
 
 const crypto = require('crypto');
 
+const getMpq = require('./getMpq');
 
-function createClient(options) {
-  const client = new Client(options);
-  const client2 = new Client2(options); // TODO : 4 param ???
+
+function createClient({host, port}) {
+  const client = new Client({host, port});
   const key1 = fs.readFileSync('./key1');
   const key2 = fs.readFileSync('./key2');
 
@@ -47,106 +46,83 @@ function createClient(options) {
   });
 
 
+  function writeAuthCheck() {
+    client.write("SID_AUTH_CHECK",{
+      "clientToken": 1520917560,
+      "exeVersion": 16780544,
+      "exeHash": 1666909528,
+      "numberOfCDKeys": 2,
+      "spawnKey": 0,
+      "cdKeys": [
+        {
+          "keyLength": 26,
+          "keyProductValue": 24,
+          "keyPublicValue": 10916470,
+          "unknown": 0,
+          "hashedKeyData": key1
+        },
+        {
+          "keyLength": 26,
+          "keyProductValue": 25,
+          "keyPublicValue": 6187878,
+          "unknown": 0,
+          "hashedKeyData": key2
+        }
+      ],
+      "exeInformation": "Game.exe 10/18/11 20:48:14 65536",
+      "keyOwnerName": "sonlight"
+    });
+  }
+
+
   client.on("SID_AUTH_INFO",({logonType,serverToken,udpValue,mpqFiletime,mpqFilename,valuestring}) => {
-    client2.connect();
-
-    client2.on('connect', () => {
-      //'connect' listener
-      console.log('connected to server!');
-      //client.write('world!\r\n');
-      client2.socket.write(Buffer.from("02","hex")); // This initialises a BNFTP file download conversation
-
-      console.log("Downloading mpq : ",mpqFilename);
-
-      client2.write('FILE_TRANSFER_PROTOCOL',{
-        requestLength:47,
-        protocolVersion:256,
-        platformId:client.platformId,
-        productId:client.productId,
-        bannerId:0,
-        bannerFileExtension:0,
-        startPositionInFile:0,
-        filetimeOfLocalFile:mpqFiletime,
-        fileName : mpqFilename
-      });
-
-    client2.on('FILE_TRANSFER_PROTOCOL', (data) => {
-      console.log(data);
-      client.write("SID_AUTH_CHECK",{
-            "clientToken": 1520917560,
-            "exeVersion": 16780544,
-            "exeHash": 1666909528,
-            "numberOfCDKeys": 2,
-            "spawnKey": 0,
-            "cdKeys": [
-              {
-                "keyLength": 26,
-                "keyProductValue": 24,
-                "keyPublicValue": 10916470,
-                "unknown": 0,
-                "hashedKeyData": key1
-              },
-              {
-                "keyLength": 26,
-                "keyProductValue": 25,
-                "keyPublicValue": 6187878,
-                "unknown": 0,
-                "hashedKeyData": key2
-              }
-            ],
-            "exeInformation": "Game.exe 10/18/11 20:48:14 65536",
-            "keyOwnerName": "sonlight"
-          });
-
-        client.on('SID_AUTH_CHECK',({result,additionalInformation}) => {
-            if(0 == result) {
-                console.log("Correct keys");
-                client.write('SID_GETFILETIME',{
-                  requestId:2147483652,
-                  unknown:0,
-                  filename:"bnserver-D2DV.ini"
-                });
-                client.write('SID_LOGONRESPONSE2',{
-                  clientToken:18226750,
-                    serverToken:1515471831,
-                    passwordHash:[ // TODO : use sha1 algorithm
-                        209,
-                        191,
-                        200,
-                        138,
-                        138,
-                        129,
-                        17,
-                        9,
-                        224,
-                        15,
-                        179,
-                        176,
-                        152,
-                        190,
-                        71,
-                        27,
-                        95,
-                        95,
-                        94,
-                        84
-                    ],
-                    username:"urukubal"
-                })
-            }
-        });
-
-        client.on('SID_LOGONRESPONSE2',({status,additionalInformation}) => {
-          console.log(status == 0 ? "Success" : status == 1 ? "Account doesn't exist" : status == 2 ? "Invalid password" : "Account closed");
-        });
-    });
-
-    });
+    getMpq(host, port, mpqFiletime, mpqFilename, client.platformId, client.productId, writeAuthCheck);
   });
 
 
 
+  client.on('SID_AUTH_CHECK',({result,additionalInformation}) => {
+    if(0 === result) {
+      console.log("Correct keys");
+      client.write('SID_GETFILETIME',{
+        requestId:2147483652,
+        unknown:0,
+        filename:"bnserver-D2DV.ini"
+      });
+      client.write('SID_LOGONRESPONSE2',{
+        clientToken:18226750,
+        serverToken:1515471831,
+        passwordHash:new Buffer([ // TODO : use sha1 algorithm
+          209,
+          191,
+          200,
+          138,
+          138,
+          129,
+          17,
+          9,
+          224,
+          15,
+          179,
+          176,
+          152,
+          190,
+          71,
+          27,
+          95,
+          95,
+          94,
+          84
+        ]),
+        username:"urukubal"
+      })
+    }
+  });
 
+
+  client.on('SID_LOGONRESPONSE2',({status,additionalInformation}) => {
+    console.log(status === 0 ? "Success" : status === 1 ? "Account doesn't exist" : status === 2 ? "Invalid password" : "Account closed");
+  });
 
 
   client.connect();
---
+++ getMpq.js
@@ -0,0 +1,38 @@
+const Client2 = require('./client2');
+
+
+function getMpq(host, port, mpqFiletime, mpqFilename, platformId, productId, cb) {
+  const client2 = new Client2({host, port}); // TODO : 4 param ???
+
+  client2.connect();
+
+  client2.on('connect', () => {
+    //'connect' listener
+    console.log('connected to server!');
+    //client.write('world!\r\n');
+    client2.socket.write(Buffer.from("02","hex")); // This initialises a BNFTP file download conversation
+
+    console.log("Downloading mpq : ",mpqFilename);
+
+    client2.write('FILE_TRANSFER_PROTOCOL',{
+      requestLength:47,
+      protocolVersion:256,
+      platformId:platformId,
+      productId:productId,
+      bannerId:0,
+      bannerFileExtension:0,
+      startPositionInFile:0,
+      filetimeOfLocalFile:mpqFiletime,
+      fileName : mpqFilename
+    });
+  });
+
+
+  client2.on('FILE_TRANSFER_PROTOCOL', (data) => {
+    console.log(data);
+    cb(null, data);
+
+  });
+}
+
+module.exports = getMpq;
