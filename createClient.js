c018b3701a56088e7682e50a78b00cd1316d2206
Romain Beaumont <romain.rom1@gmail.com>
2018-07-14 22:35:08 +0200

    work until compressed

--- createClient.js
+++ createClient.js
@@ -329,25 +329,16 @@ function createClient({username, password, host, port, character, gameName, game
           locale: 0,
           characterName: asciiCharName
         });
-
-
-
-        // ???
-        /*
-        clientD2gs.on('D2GS_LOGONRESPONSE',(data) => {
-            clientD2gs.write('D2GS_PING', {
-                tickCount: 1
-            });
-
-          clientD2gs.write('D2GS_ENTERGAMEENVIRONMENT', {});
-        });*/
-
-        clientD2gs.on('D2GS_COMPSTARTGAME',(data) => {
-
-          clientD2gs.write('D2GS_ENTERGAMEENVIRONMENT', {});
-        });
       });
 
+
+      clientD2gs.on('D2GS_LOGONRESPONSE',(data) => {
+        clientD2gs.write('D2GS_ENTERGAMEENVIRONMENT', {});
+      });
+
+      clientD2gs.on('D2GS_COMPSTARTGAME',(data) => {
+        clientD2gs.write('D2GS_ENTERGAMEENVIRONMENT', {});
+      });
 
 
       clientD2gs.on('D2GS_PING', ({tickCount}) => {
