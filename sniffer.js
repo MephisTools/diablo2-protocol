fc59165ae3f012a62bb3d3d7dfa54435bb3f839c
louis030195 <louis.beaumont@ynov.com>
2018-12-29 23:11:14 +0100

    Improve sniffer

--- sniffer.js
+++ sniffer.js
@@ -13,10 +13,36 @@ const ProtoDef = require("protodef").ProtoDef;
 
 
 const d2gsProto = require('./data/d2gs');
-
 const d2gs = new ProtoDef();
 //d2gs.addProtocol(d2gsProto,["toServer"]);
 d2gs.addProtocol(d2gsProto,["toClient"]);
+
+const mcp = require('./data/mcp');
+
+const mcpToServer = new ProtoDef();
+mcpToServer.addProtocol(mcp,["toServer"]);
+
+
+const mcpToClient = new ProtoDef();
+mcpToClient.addProtocol(mcp,["toClient"]);
+
+const sid = require('./data/sid');
+
+const sidToServer = new ProtoDef();
+sidToServer.addProtocol(sid,["toServer"]);
+
+
+const sidToClient = new ProtoDef();
+sidToClient.addProtocol(sid,["toClient"]);
+
+const bnftp = require('./data/bnftp');
+
+const bnftpToServer = new ProtoDef();
+bnftpToServer.addProtocol(bnftp,["toServer"]);
+
+
+const bnftpToClient = new ProtoDef();
+bnftpToClient.addProtocol(bnftp,["toClient"]);
 
 const {createSplitter, createFramer} = require('./splitter');
 
@@ -44,40 +70,112 @@ toClientParser.on('data', ({data}) => {
     console.info("received compressed packet", name, params);
 })
 
+let clientPortSid = null
+let clientPortBnFtp = null
+let compression = true
+
+const trackedPorts = new Set(['6112', '4000', '6113'])
+
 // tracker emits sessions, and sessions emit data
 tcp_tracker.on("session", function (session) {
-    if(session.src_name.split(':')[1] == '4000' || session.dst_name.split(':')[1] == '4000')
-        console.log("Start of TCP session between " + session.src_name + " and " + session.dst_name);
+    const srcPort = session.src_name.split(':')[1]
+    const dstPort = session.dst_name.split(':')[1]
+    console.log({srcPort, dstPort})
+    if(!trackedPorts.has(srcPort) && !trackedPorts.has(dstPort)) {
+        return
+    }
+    console.log("truc", {srcPort, dstPort})
+    
+    if(dstPort == '6112') {
+        if(clientPortSid === null) {
+            clientPortSid = srcPort
+        } else {
+            clientPortBnFtp = srcPort
+        }
+    }
+    if(srcPort == '6112') {
+        if(clientPortSid === null) {
+            clientPortSid = dstPort
+        } else {
+            clientPortBnFtp = dstPort
+        }
+    }
     session.on("data send", function (session, data) {
-        if(session.src_name.split(':')[1] == '4000' || session.dst_name.split(':')[1] == '4000')
+        const srcPort = session.src_name.split(':')[1]
+        const dstPort = session.dst_name.split(':')[1]
+        if(srcPort == '4000')
         {
-            console.log('allo');
-            try{
-
-                //x = d2gs.parsePacketBuffer("packet", data).data
-                splitter.write(data);
-                //console.log(JSON.stringify(x, null, 2));
-                /*
-                if(x.name == 'D2GS_RUNTOLOCATION'){
-                    setInterval(
-                        function(){ session.inject(data) },
-                        5000
-                      );
-                }*/
-
-
-            }catch(error) {
-                console.error("Error : " + error);
+            console.log('allo')
+            try {
+                if(!compression) 
+                {
+                    if(data[0] !== 0xaf)
+                        data = data.slice(1);
+            
+                    const parsed = protoToClient.parsePacketBuffer("packet",data).data;
+            
+                    const {name,params} = parsed;
+                    console.info("received uncompressed packet", name, params);
+                    if(name === 'D2GS_NEGOTIATECOMPRESSION' && params.compressionMode !== 0){
+                        compression = true
+                    }
+                    }
+                    else {
+                        splitter.write(data);
+                    }
+            } catch (error) {
+                console.log(error)
             }
         }
+        
+        if(dstPort == '6113')
+        {
+            if(data.toString('hex') != '01')
+                console.log("mcpToServer : ", mcpToServer.parsePacketBuffer('packet', data))
+        }
+
+        if(srcPort == '6113')
+        {
+            console.log("mcpToClient : ", mcpToClient.parsePacketBuffer('packet', data))
+        }
+
+        if(dstPort == '6112' || srcPort == '6112' ) {
+            console.log({src:srcPort , dst:dstPort})
+        }
+
+        if(dstPort == '6112' && srcPort === clientPortSid)
+        {
+            try {
+                console.log("sidToServer : ", sidToServer.parsePacketBuffer('packet', data),null,2)
+            } catch(err) {
+                console.log(err)
+            }
+        }
+
+        if(srcPort == '6112' && dstPort === clientPortSid)
+        {
+            console.log("sidToClient : ", sidToClient.parsePacketBuffer('packet', data))
+        }
+
+        
+        if(dstPort == '6113' && srcPort === clientPortBnFtp)
+        {
+            console.log("bnftpToServer : ", bnftpToServer.parsePacketBuffer('packet', data))
+        }
+
+        if(srcPort == '6113' && dstPort === clientPortBnFtp)
+        {
+            console.log("bnftpToClient : ", bnftpToClient.parsePacketBuffer('packet', data))
+        }
+
     });
     session.on("data recv", function (session, data) {
-        //if(session.src_name.split(':')[1] == '4000' || session.dst_name.split(':')[1] == '4000')
+        //if(srcPort == '4000' || dstPort == '4000')
         //    console.log("data received " + session.recv_bytes_payload + " + " + data.length + " bytes");
         });
     session.on("end", function (session) {
-        if((session.src_name.split(':')[1] == '4000' || session.dst_name.split(':')[1] == '4000') ||
-            (session.src_name.split(':')[1] == '6112' || session.dst_name.split(':')[1] == '6112'))
+        if((srcPort == '4000' || dstPort == '4000') ||
+            (srcPort == '6112' || dstPort == '6112'))
         {
             console.log("End of TCP session between " + session.src_name + " and " + session.dst_name);
             console.log("Set stats for session: ", session.session_stats());
