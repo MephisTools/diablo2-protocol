1b2ded9349da1c6be882d8e6cc0082f3950144c8
Romain Beaumont <romain.rom1@gmail.com>
2019-01-11 22:20:10 +0100

    create simple raw-d2gs proxy

---
+++ examples/proxy.js
@@ -0,0 +1,58 @@
+const { createClientDiablo, ServerDiablo, createServerSid, createServerMcp, ServerD2gs } = require('..')
+
+if (process.argv.length !== 9) {
+  console.log('Usage : node bot.js <username> <password> <character> <gamename> <gamepasswd> <gameserver> <externalHost>')
+  process.exit(1)
+}
+
+const username = process.argv[2]
+const password = process.argv[3]
+const character = process.argv[4]
+const gameName = process.argv[5]
+const gamePassword = ''
+const gameServer = process.argv[7]
+const externalHost = process.argv[8]
+
+const host = '127.0.0.1'
+
+const serverDiablo = new ServerDiablo()
+
+const serverSid = createServerSid(host, externalHost)
+
+serverDiablo.setServerSid(serverSid)
+
+const serverMcp = createServerMcp(host, externalHost)
+
+serverDiablo.setServerMcp(serverMcp)
+
+// Connect to battlenet
+function createServerD2gs (host) {
+  const portD2gs = 4000
+  const serverD2gs = new ServerD2gs()
+  serverD2gs.listen(host, portD2gs)
+  serverD2gs.on('connection', async clientD2gsServer => {
+    console.log('new client d2gs', clientD2gsServer.socket.address())
+
+    const clientDiablo = await createClientDiablo({
+      host: '198.98.54.85',
+      username,
+      password
+    })
+    clientDiablo.on('clientD2gsReady', clientD2gsClient => {
+      clientD2gsClient.socket.on('data', data => clientD2gsServer.socket.write(data))
+      clientD2gsServer.socket.on('data', data => clientD2gsClient.socket.write(data))
+    })
+
+    await clientDiablo.selectCharacter(character)
+    await clientDiablo.createGame(gameName, gamePassword, gameServer, 0)
+    console.log('Has joined the game')
+  })
+  return serverD2gs
+}
+
+const serverD2gs = createServerD2gs(host)
+
+serverDiablo.setServerD2gs(serverD2gs)
+
+serverDiablo.on('connection', client => {
+})
--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const d2gsProtocol = require('./data/d2gs')
 const d2gsReader = require('./lib/utils/d2gsSpecialReader')
 const getHash = require('./lib/utils/getHash')
 const createServerDiablo = require('./lib/server/createServerDiablo')
+const ServerDiablo = require('./lib/server/serverDiablo')
+const createServerSid = require('./lib/server/createServerSid')
+const createServerMcp = require('./lib/server/createServerMcp')
+const ServerD2gs = require('./lib/server/serverD2gs')
 
 module.exports = {
   createClientDiablo,
@@ -21,5 +25,9 @@ module.exports = {
   d2gsProtocol,
   d2gsReader,
   getHash,
-  createServerDiablo
+  createServerDiablo,
+  createServerSid,
+  createServerMcp,
+  ServerD2gs,
+  ServerDiablo
 }
