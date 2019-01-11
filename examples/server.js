371e59fa664716958a7f520a0899e4c1c0c09b0a
Romain Beaumont <romain.rom1@gmail.com>
2019-01-11 19:53:46 +0100

    fix server, now goes up to mcp

--- examples/server.js
+++ examples/server.js
@@ -1,9 +1,11 @@
 const createServerDiablo = require('..').createServerDiablo
 
-const serverDiablo = createServerDiablo('127.0.0.1')
+if (process.argv.length !== 3) {
+  console.log('Usage : node bot.js <externalHost>')
+  process.exit(1)
+}
+
+const serverDiablo = createServerDiablo('127.0.0.1', process.argv[2])
 
 serverDiablo.on('connection', client => {
-  client.on('packet', ({ name, params }) => {
-    console.log(name, params)
-  })
 })
